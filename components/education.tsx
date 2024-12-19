f7eb9bfd69e13985f3485b5775f791c34fbdb16f
bakar31 <abubakar1808031@gmail.com>
2024-12-19 19:27:28 +0600

    update education section

--- components/education.tsx
+++ components/education.tsx
@@ -5,6 +5,7 @@ import {
   CardDescription,
   CardContent,
 } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
 
 interface Education {
   degree: string;
@@ -19,36 +20,62 @@ const educationData: Education[] = [
     institution: "Rajshahi University of Engineering & Technology",
     duration: "January 2019 - May 2024",
     details: [
-      "CGPA: 3.79/4.00",
+      "CGPA: 3.79/4.00 (3rd)",
       "Relevant Coursework: Software Engineering, Artificial Intelligence, Robotics, Machine Learning Algorithms, Numerical Analysis & Statistics, Digital Signal Processing Vision, Embedded Systems, Automation",
     ],
   },
   {
     degree: "HSC, Science",
     institution:
-      "President Professor Dr. Iajuddin Ahmed Residential Model School and College",
+      "President Prof. Dr. Iajuddin Ahmed Residential Model School & College",
     duration: "March 2016 - April 2018",
-    details: [],
+    details: [
+      "Grade: 5.00/5.00",
+      "Best Student Award",
+      "1st runner-up in the district-level quiz competition (2017)",
+      "1st runner-up in the district-level project showcasing competition (2017)",
+    ],
   },
-  // {
-  //   degree: "SSC",
-  //   institution: "Bajrajogini J.K High School",
-  //   duration: "January 2011 - April 2016",
-  //   details: [],
-  // },
+  {
+    degree: "SSC",
+    institution: "Bajrajogini J.K High School",
+    duration: "January 2011 - April 2016",
+    details: [
+      "Grade: 5.00/5.00",
+      "Best Student Award (4x)",
+      "Best Science Student of the District (1x)",
+      "Best Science Student of the Subdistrict (1x)",
+    ],
+  },
 ];
 
 export function Education() {
   return (
     <section id="education" className="py-20 bg-gray-50">
       <h2 className="mb-8 text-3xl font-bold text-center">Education</h2>
-      <div className="max-w-4xl mx-auto space-y-6">
+      <div className="max-w-5xl mx-auto space-y-6">
         {educationData.map((edu, index) => (
           <Card key={index}>
             <CardHeader>
-              <CardTitle>{edu.degree}</CardTitle>
-              <CardDescription>{edu.institution}</CardDescription>
-              <CardDescription>{edu.duration}</CardDescription>
+              <CardTitle>
+                <span className="px-2 py-1 rounded-md font-bold text-blue-800">
+                  {edu.degree}
+                </span>
+              </CardTitle>
+              <CardDescription>
+                <Badge
+                  variant="outline"
+                  className="bg-white text-sm px-2 py-1 rounded-full font-bold text-orange-800 mr-1"
+                >
+                  {edu.institution}
+                </Badge>
+                <Badge
+                  variant="outline"
+                  className="bg-white text-sm px-2 py-1 rounded-full font-bold text-purple-800 mx-1"
+                >
+                  {edu.duration}
+                </Badge>
+              </CardDescription>
             </CardHeader>
             {edu.details.length > 0 && (
               <CardContent>
