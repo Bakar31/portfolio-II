12f98d2a98bcc7a34e3a29f4b3209f1ffcc94cca
bakar31 <abubakar1808031@gmail.com>
2024-12-19 19:20:42 +0600

    update experience section

--- components/experience.tsx
+++ components/experience.tsx
@@ -5,54 +5,78 @@ import {
   CardDescription,
   CardContent,
 } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
 
 interface ExperienceItem {
   title: string;
   company: string;
   duration: string;
+  type: string;
+  location: string;
   description: string[];
 }
 
 const experiences: ExperienceItem[] = [
   {
     title: "Machine Learning Engineer",
-    company: "Sazim",
-    duration: "March 2024 - Present",
+    company: "Sazim Tech Ltd.",
+    duration: "March 2023 - Present",
+    type: "Full-time",
+    location: "Remote",
     description: [
-      "Collaborating with cross-functional teams to develop LLM-based applications",
-      "Utilizing OpenAI, Gemini, and Anthropic APIs",
-      "Implementing Retrieval-Augmented Generation (RAG) with local vector databases",
-      "Developing enterprise local LLM solutions for private infrastructure",
+      "Worked on LLM-based application integrating LLMs using OpenAI, Gemini, and Anthropic APIs, with RAG supported by vector database.",
+      "Designed data models, created API endpoints, and integrated backend services with frontend interfaces for efficient application performance.",
+      "Conducted prompt engineering and LLM response evaluation, achieving significant improvements in model performance.",
+      "Built applications utilizing open-source and proprietary LLMs for deployment on private infrastructure with a focus on security and scalability.",
+      "Optimized deployment processes using Docker to ensure consistent and reliable containerized environments.",
+      "Authored technical decision documents to justify architectural and development choices.",
+      "Translated client requirements into stories and epics.",
     ],
   },
   {
     title: "Trainee Engineer",
-    company: "Sazim",
+    company: "Sazim Tech Ltd.",
     duration: "October 2023 - February 2024",
+    type: "Trainee Engineer",
+    location: "Hybrid",
     description: [
-      "Developed end-to-end RAG application with open-source LLMs",
-      "Created two full-stack applications following best practices",
-      "Learned software engineering best practices under senior mentorship",
+      "Developed an end-to-end RAG application using open-source LLMs and a database with over 3 million samples. Utilized Next.js for the frontend, and LangChain with FastAPI for the backend.",
+      "Developed multiple full-stack applications following industry-standard principles and coding guidelines.",
+      "Gained hands-on experience in software engineering by working closely with senior software engineers to learn best practices.",
+    ],
+  },
+  {
+    title: "Machine Learning Researcher and Engineer",
+    company: "Intelsense AI Limited",
+    duration: "March 2023 - September 2023",
+    type: "Part-time",
+    location: "Remote",
+    description: [
+      "Created Rasa-based chatbots for various use cases, including multiple banks, restaurants, and a mobile operator company, for proof-of-concept and pilot implementations.",
+      "Collaborated on the development of an automatic speech recognition (ASR) tool for the Bengali language.",
+      "Data Annotation Team Leader",
     ],
   },
   {
     title: "Machine Learning Researcher and Engineer Intern",
     company: "Intelsense AI Limited",
     duration: "September 2022 - March 2023",
+    type: "Internship",
+    location: "Remote",
     description: [
-      "Developed Rasa-powered multilingual restaurant chatbots",
-      "Worked on ASR tool development for Bengali Language",
-      "Led the Data Annotation Team",
+      "Developed multilingual restaurant chatbots using Rasa, capable of understanding English",
+      "Banglish, and Bangla inputs. Implemented features such as menu display, order processing, reservations, promotions, complaint handling, and FAQs, supported by a dynamic database for improved accuracy.",
     ],
   },
   {
     title: "Data Science Apprentice",
     company: "Cramstack Ltd.",
     duration: "November 2021 - May 2022",
+    type: "Apprenticeship",
+    location: "Remote",
     description: [
-      "Created high-functioning OCR tool for restaurants",
-      "Responsible for pre-processing and cleaning large, complex datasets",
-      "Conducted research on Text Summarization algorithms",
+      "Contributed to the development of a high-functioning OCR tool for a restaurant, analyzing OCR libraries and performing image processing.",
+      "Cleaned and pre-processed large, complex datasets from various clients, ensuring data accuracy and attention to detail.",
     ],
   },
 ];
@@ -61,16 +85,44 @@ export function Experience() {
   return (
     <section id="experience" className="py-20">
       <h2 className="mb-8 text-3xl font-bold text-center">Experience</h2>
-      <div className="max-w-4xl mx-auto space-y-6">
+      <div className="max-w-5xl mx-auto space-y-6">
         {experiences.map((exp, index) => (
           <Card key={index}>
             <CardHeader>
-              <CardTitle>{exp.title}</CardTitle>
+              <CardTitle>
+                <span className=" px-2 py-1 rounded-md font-bold text-blue-800">
+                  {exp.title}
+                </span>
+              </CardTitle>
               <CardDescription>
-                {exp.company} | {exp.duration}
+                <Badge
+                  variant="outline"
+                  className="bg-white text-sm px-2 py-1 rounded-full font-bold text-orange-800 mr-1"
+                >
+                  {exp.company}
+                </Badge>
+                <Badge
+                  variant="outline"
+                  className="bg-white text-sm px-2 py-1 rounded-full font-bold text-purple-800 mx-1"
+                >
+                  {exp.duration}
+                </Badge>
+                <Badge
+                  variant="outline"
+                  className="text-sm bg-white text-green-700 border-green-300 rounded-full mx-1"
+                >
+                  {exp.type}
+                </Badge>{" "}
+                <Badge
+                  variant="outline"
+                  className="text-sm bg-white text-yellow-700 border-yellow-300 rounded-full ml-1"
+                >
+                  {exp.location}
+                </Badge>
               </CardDescription>
             </CardHeader>
             <CardContent>
+              <p className="text-sm text-gray-700">{exp.company}</p>
               <ul className="list-disc list-inside">
                 {exp.description.map((item, i) => (
                   <li key={i} className="text-sm text-gray-700">
